03da8022ce4f72945283241d424efb8814c65f4c
Vance <thevancetan@gmail.com>
2021-05-31 18:13:05 +0800

    Added Stripe and Radix packages

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,27 @@ import '@/css/tailwind.css'
 
 import { DefaultSeo } from 'next-seo'
 import Head from 'next/head'
+import { loadStripe } from '@stripe/stripe-js'
+import { CartProvider } from 'use-shopping-cart'
+import { IdProvider } from '@radix-ui/react-id'
 
 import { SEO } from '@/components/SEO'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <DefaultSeo {...SEO} />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </>
+    <CartProvider mode="checkout-session" stripe={stripePromise} currency="SGD">
+      <IdProvider>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        <DefaultSeo {...SEO} />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </IdProvider>
+    </CartProvider>
   )
 }
